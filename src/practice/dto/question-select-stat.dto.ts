78b882100071cf87f0a24735ce8bf15a4dc9c0a3
Mainak Karmakar <mainakkarmakar.lg@gmail.com>
2025-07-03 19:17:43 +0530

    first commit

---
+++ src/practice/dto/question-select-stat.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEnum } from 'class-validator';
+
+export class QuestionSelectStatDto {
+  @ApiProperty()
+  @IsEnum(['easy', 'medium', 'hard'])
+  difficulty: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  flagged: boolean;
+
+  @ApiProperty()
+  @IsBoolean()
+  incorrect: boolean;
+
+  @ApiProperty()
+  @IsBoolean()
+  unattempted: boolean;
+}
